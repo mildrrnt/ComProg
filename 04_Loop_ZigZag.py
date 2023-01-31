c75a28165030ed1a6ab9d57d0eb934c9b2e53256
Worranittha H <86171659+mildrrnt@users.noreply.github.com>
2023-01-31 21:49:00 +0700

    Update

--- 04_Loop_ZigZag.py
+++ 04_Loop_ZigZag.py
@@ -1,14 +1,8 @@
 n = int(input())
 li1 = [] #start at L side
 li2 = [] #start at R side
-for i in range(1,n+2):
+for i in range(1,n+1):
     s = input()
-    if s == "Zig-Zag":
-        print(min(li1),max(li2)) #min of L, max of R
-        break
-    elif s == "Zag-Zig":
-        print(min(li2),max(li1)) #min of R, max of L
-        break
     x = [int(i) for i in s.split()] #convert to int
     if i%2 !=0: #check if the count of line is even or odd
         li1.append(x[0])
@@ -16,3 +10,8 @@ for i in range(1,n+2):
     else:
         li1.append(x[1])
         li2.append(x[0])
+s = input()
+if s == "Zig-Zag":
+    print(min(li1),max(li2)) #min of L, max of R
+elif s == "Zag-Zig":
+    print(min(li2),max(li1)) #min of R, max of L
