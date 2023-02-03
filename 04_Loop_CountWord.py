6a88977dacfe698e735d76070664cd0c86494002
Worranittha H <86171659+mildrrnt@users.noreply.github.com>
2023-02-03 15:19:44 +0700

    shorthen code

--- 04_Loop_CountWord.py
+++ 04_Loop_CountWord.py
@@ -3,15 +3,8 @@ key = input()
 sen = input()
 
 for ch in spe:
-    if ch in sen:
-        sen = sen.replace(ch," ") #replace each char with space
+    sen = sen.replace(ch," ") #replace each char with space
 
 sen = sen.split() #split into list / does not count space
-count = 0
-
-for s in sen:
-    if s == key: #check if the word is keyword or not
-        count += 1
         
-
-print(count)
+print(sen.count(key))
