dc331e3ea7e9a0d0f1b537105c19a91ea305f2e9
Worranittha H <86171659+mildrrnt@users.noreply.github.com>
2023-02-08 19:51:07 +0700

    Update 04_Loop_CountWord.py

--- 04_Loop_CountWord.py
+++ 04_Loop_CountWord.py
@@ -4,5 +4,7 @@ sen = input()
 
 for ch in spe:
     sen = sen.replace(ch," ") #replace each char with space
+
+sen = sen.split() #split into list / does not count space
         
 print(sen.count(key))
