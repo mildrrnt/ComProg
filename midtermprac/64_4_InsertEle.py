fbc50f6525f6209045518b7431e7b7c2a7ce8c82
Worranittha H <86171659+mildrrnt@users.noreply.github.com>
2023-02-27 16:13:54 +0700

    Eidt 64_4_InsertEle.py

--- midtermprac/64_4_InsertEle.py
+++ midtermprac/64_4_InsertEle.py
@@ -1,8 +1,9 @@
-data =  [['A102', 50], ['A103', 40], ['B014', 40], ['B109', 30]]
+data =  [['A102', 20], ['A014', 40], ['B104', 40], ['B109', 30]]
 
 n = input().split(',')
 n[1] = int(n[1])
 
 data.append(n)
+data.sort()
 data.sort(key = lambda x: x[1], reverse=True)
 print(data)
