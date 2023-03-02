a2b4f07a57174baae606f4d3f5e62bf0a483d9cf
Worranittha H <86171659+mildrrnt@users.noreply.github.com>
2023-03-02 22:52:19 +0700

    Edit 04_Loop_RLE.py

--- 04_Loop_RLE.py
+++ 04_Loop_RLE.py
@@ -1,7 +1,6 @@
 l = input()
 n = ""
-li = []
-
+'''
 for d in l:
     if not d in n and n != "": #new character
         li.append(n)
@@ -14,3 +13,13 @@ for s in li:
     x += s[0]+" "+ str(len(s))+" "
 
 print(x[:-1]) #exclude the last space
+
+'''
+c = 1
+for i in range(1,len(l)):
+    if l[i-1] == l[i]:
+        c += 1
+    else:
+        print(l[i-1],c,end=" ")
+        c = 1
+print(l[-1],c)
